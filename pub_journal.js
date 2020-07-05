c000e211ffe5474f416d683b8bd579f36848f3b3
drazenb <>
2020-07-05 10:26:06 +0900

    Japanese added

---
+++ pub_journal.js
@@ -0,0 +1,18 @@
+document.write(' \
+<ul> \
+  <li>K. Du, T. Kaczmarek, D. Brščić, T. Kanda, <span class="italic">Recognition of Rare Low-Moral Actions Using Depth Data</span>, Sensors, Vol. 20, No. 10, 2758, 2020</li> \
+  <li>F. Zanlungo, Z. Yücel, D. Brščić, T. Kanda, N. Hagita, <span class="italic">Intrinsic group behaviour: Dependence of pedestrian dyad dynamics on principal social and personal features</span>, PLoS One, Vol. 12, No. 11, e0187253, pp. 1-26, 2017</li> \
+  <li>D. Brščić, T. Ikeda, T. Kanda, <span class="italic">Do You Need Help? A Robot Providing Information to People Who Behave Atypically</span>, IEEE Transactions on Robotics, Vol. 33, No. 2, pp. 500-506, 2017</li> \
+  <li>H. Kidokoro, T. Kanda, D. Brščić, M. Shiomi, <span class="italic">Simulation-Based Behavior Planning to Prevent Congestion of Pedestrians Around a Robot</span>, IEEE Transactions on Robotics, Vol. 31, No. 6, pp. 1419-1431, 2015</li> \
+  <li>F. Zanlungo, D. Brščić, T. Kanda, <span class="italic">Spatial-size scaling of pedestrian groups under growing density conditions</span>, Physical Review E, Vol. 91, No. 6, p. 062810, 2015</li> \
+  <li>D. Brščić, T. Kanda, <span class="italic">Changes in Usage of an Indoor Public Space: Analysis of One Year of Person Tracking</span>, IEEE Transactions on Human-Machine Systems, Vol. 45, No. 2, pp. 228-237, 2015</li> \
+  <li>D. Brščić, T. Kanda, T. Ikeda, T. Miyashita, <span class="italic">Person tracking in large public spaces using 3D range sensors</span>, IEEE Transactions on Human-Machine Systems, Vol. 43, No. 6, pp. 522-534, 2013</li> \
+  <li>X. Zang, D. Brščić, W. Wang, S. Hirche, K. Kuehnlenz, <span class="italic">Human Behavior Inspired Temporal Attention Control System for Multiple Object Tracking</span>, Journal of University of Science and Technology of China, Vol. 24, No. 9, pp. 714-722, 2012</li> \
+  <li>A. Kirsch, T. Kruse, E.A. Sisbot, R. Alami, M. Lawitzky, D. Brščić, S. Hirche, P. Basili, S. Glasauer, <span class="italic">Plan-based Control of Joint Human-Robot Activities</span>, KI - Kuenstliche Intelligenz, Vol. 24, No. 3, pp. 223-231, 2010</li> \
+  <li>M. Rambow, F. Rohrmueller, O. Kourakos, D. Brščić, D. Wollherr, S. Hirche, M. Buss, <span class="italic">A Framework for Information Distribution, Task Execution and Decision Making in Multi-Robot Systems</span>, IEICE Transactions on Information and Systems, Vol. E93-D, No. 6, pp.1352-1360, 2010</li> \
+  <li>T. Sasaki, D. Brščić, H. Hashimoto, <span class="italic">Human Observation Based Extraction of Path Patterns for Mobile Robot Navigation</span>, IEEE Transactions on Industrial Electronics, Vol.57, No.4, pp.1401-1410, 2010</li> \
+  <li>D. Brščić, H. Hashimoto, <span class="italic">Sensing in Intelligent Spaces: Joint Use of Distributed and Onboard Sensors</span>, AUTOMATIKA, Vol. 50, Nr. 3-4, pp. 205-214, 2009</li> \
+  <li>D. Brščić, H. Hashimoto, <span class="italic">Mobile Robot as Physical Agent of Intelligent Space</span>, Journal of Computing and Information Technology (CIT) , Vol. 17, Nr. 1, pp. 81-94, 2009</li> \
+  <li>D. Brščić, T. Sasaki, H. Hashimoto, <span class="italic">Tracking of Humans and Robots using Laser Range Finders</span>, SICE Journal of Control, Measurement, and System Integration (SICE JCMSI), Vol. 1, Nr. 5, pp. 383-392, 2008</li> \
+</ul> \
+');
