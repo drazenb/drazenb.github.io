f3ef64f85a2d9a98eaff22e6f2ef23aa22816d5a
unknown <drazen@i.kyoto-u.ac.jp>
2025-08-29 22:49:07 +0200

    updates

--- pub_journal.js
+++ pub_journal.js
@@ -1,5 +1,7 @@
 document.write(' \
 <ul> \
+  <li>K. Makita, D. Brščić, T. Kanda, <span class="italic">How Should a Robot do Indirect Handovers Politely?</span>, International Journal of Social Robotics, Vol. 17, pp. 1501–1515, 2025</li> \
+  <li>H.M. Shehata, Y. Liu, D. Brščić, T. Kanda, <span class="italic">Simulation-Based Machine Learning for Detection of Rarely Occurring Behaviors Based on Human Trajectories</span>, International Journal of Social Robotics, Vol. 17, pp. 1485-1499, 2025</li> \
   <li>K. Hosoda, D. Brščić, T. Kanda, <span class="italic">Interaction-aware augmented reality interface for assisting erring social robots</span>, Journal of Multimodal User Interfaces, 2025</li> \
   <li>K. Nishiwaki, D. Brščić, T. Kanda, <span class="italic">Expressing Anger with Robot for Tackling the Onset of Robot Abuse</span>, ACM Transactions on Human-Robot Interaction, 14, 1, Article 10, 23 pages, 2024</li> \
   <li>T. Shaheen, D. Brščić, T. Kanda, <span class="italic">Investigation of low-moral actions by malicious anonymous operators of avatar robots</span>, ACM Transactions on Human-Robot Interaction, 14, 1, Article 11, 34 pages, 2024</li> \
